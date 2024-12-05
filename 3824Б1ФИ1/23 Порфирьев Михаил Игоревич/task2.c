ff6052516afe69244a393c046464714f179c74ab
Higraunb <bear.porf@gmail.com>
2024-12-05 09:58:13 +0300

    Update task2.c

--- 3824Б1ФИ1/23 Порфирьев Михаил Игоревич/task2.c
+++ 3824Б1ФИ1/23 Порфирьев Михаил Игоревич/task2.c
@@ -1,6 +1,13 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
+  unsigned int c = 0;
+  c = a * b;
+  if(c >= UINI_MAX)
+    return UINI_MAX;
+  else
+    return c;
   return 0;
 }
